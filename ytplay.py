694b1ee9ccf1a320909e7be21e6cbc180bb511c2
Allen Li <darkfeline@felesatra.moe>
2016-05-25 22:30:44 -0700

    Fix stuff

--- ytplay.py
+++ ytplay.py
@@ -9,67 +9,114 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(level='DEBUG')
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(url_player(loop))
+    loop.run_until_complete(url_player())
     loop.close()
 
 
-async def url_player(loop):
+async def url_player():
+    loop = asyncio.get_event_loop()
     input_stream = await AsyncTextStream.make(loop, sys.stdin)
-    url_channel = Channel()
     buffer_channel = Channel()
-    tasks = []
-    tasks.append(read_urls(input_stream, url_channel))
-    tasks.append(buffer_streams(url_channel, buffer_channel))
-    tasks.append(play_streams(buffer_channel))
-    await asyncio.gather(*tasks)
+    play_channel = Channel()
+    await asyncio.gather(
+        read_urls(input_stream, buffer_channel),
+        buffer_streams(buffer_channel, play_channel),
+        play_streams(play_channel),
+    )
 
 
-async def read_urls(input_stream, url_channel):
-    async with url_channel:
+async def read_urls(input_stream, buffer_channel):
+    async with buffer_channel:
         async for video_url in input_stream:
             if not video_url:
                 break
             print(video_url)
-            await url_channel.put(video_url)
+            song = Song(video_url)
+            await buffer_channel.put(song)
 
 
-async def buffer_streams(url_channel, buffer_channel):
-    async with buffer_channel:
+async def buffer_streams(buffer_channel, play_channel):
+    async with play_channel:
         while True:
-            video_url = await url_channel.get()
-            if video_url is Channel.DONE:
+            song = await buffer_channel.get()
+            if song is Channel.DONE:
                 break
-            reader, writer = os.pipe()
-            proc = await buffer_one_stream(video_url, writer)
-            await buffer_channel.put(reader)
+            await song.buffer()
+            await play_channel.put(song)
 
 
-def buffer_one_stream(video_url, buffer):
-    logger.info('Buffering %s', video_url)
-    return asyncio.create_subprocess_exec(
-        'youtube-dl', '-q', '-o', '-', video_url,
-        stdout=buffer,
-    )
-
-
-async def play_streams(buffer_channel):
+async def play_streams(play_channel):
     while True:
-        buffer = await buffer_channel.get()
-        if buffer is Channel.DONE:
+        song = await play_channel.get()
+        if song is Channel.DONE:
             break
-        proc = await play_one_stream(buffer)
-        await proc.wait()
+        await song.play()
 
 
-def play_one_stream(buffer):
-    logger.info('Playing %s', buffer)
-    return asyncio.create_subprocess_exec(
-        'mpv', '--no-terminal', '--no-video', '-',
-        stdin=buffer,
-    )
+class Song:
+
+    def __init__(self, url):
+        self.url = url
+        self.buffer_proc = None
+        self.buffer_out = None
+        self.buffer_in = None
+        self.player_proc = None
+
+    async def buffer(self):
+        self.buffer_out, self.buffer_in = os.pipe()
+        logger.info('Buffering %s', self)
+        self.buffer_proc = await self.youtube_dl(self.url, self.buffer_in)
+        loop = asyncio.get_event_loop()
+        loop.create_task(wait_for_proc(self.buffer_proc, self.buffer_in))
+
+    @staticmethod
+    async def youtube_dl(video_url, pipe):
+        return await asyncio.create_subprocess_exec(
+            'youtube-dl', '-q', '-o', '-', video_url,
+            stdout=pipe,
+        )
+
+    async def play(self):
+        logger.info('Playing %s', self)
+        self.player_proc = await self.start_player(self.buffer_out)
+        await self.player_proc.wait()
+        os.close(self.buffer_out)
+        logger.info('Player exited %s', self)
+
+    @staticmethod
+    async def start_player(pipe):
+        return await asyncio.create_subprocess_exec(
+            'mpv', '--no-terminal', '--no-video', '-',
+            stdin=pipe,
+        )
+
+    def __repr__(self):
+        return 'Song({!r})'.format(self.url)
+
+    def __str__(self):
+        return 'Song {!s} {}'.format(self.url, self.status)
+
+    @property
+    def status(self):
+        if self.buffer_proc is None:
+            return 'Queued'
+        elif self.player_proc is None:
+            return 'Buffering'
+        elif self.player_proc.returncode < 0:
+            return 'Playing'
+        else:
+            return 'Finished'
+
+
+async def wait_for_proc(proc, *pipes):
+    await proc.wait()
+    for pipe in pipes:
+        os.close(pipe)
 
 
 class AsyncStream:
+
+    """Wrap a regular file object for async access."""
 
     @classmethod
     async def make(cls, loop, pipe):
@@ -100,6 +147,8 @@ class AsyncTextStream(AsyncStream):
 
 
 class Channel(asyncio.Queue):
+
+    """Implement Golang-like channel closing on a queue."""
 
     DONE = object()
 
