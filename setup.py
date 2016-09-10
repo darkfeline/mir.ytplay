3dc5c97c3dc95e64b43afb0a7d046eab4f6d69fa
Allen Li <darkfeline@felesatra.moe>
2016-09-10 02:09:41 -0700

    Repackage

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='ytplay',
+    name='mir.ytplay',
     version='0.1.0',
+    description='Stream music from YouTube.',
+    url='',
+    author='Allen Li',
+    author_email='darkfeline@felesatra.moe',
+    license='',
+
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'ytplay = ytplay:main',
+            'ytplay = mir.ytplay:main',
         ],
     },
 
-    author='Allen Li',
-    author_email='darkfeline@felesatra.moe',
-    description='',
-    license='',
-    url='',
 )
